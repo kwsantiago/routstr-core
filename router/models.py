811e313652a213e5c61f70118c6c8ff08d7f3187
9qeklajc <9qeklajc>
2025-06-15 21:09:24 +0200

    format

--- router/models.py
+++ router/models.py
@@ -134,7 +134,7 @@ async def update_sats_pricing() -> None:
         except asyncio.CancelledError:
             break
         except Exception as e:
-            print('Error updating sats pricing: ', e)
+            print("Error updating sats pricing: ", e)
         try:
             await asyncio.sleep(10)
         except asyncio.CancelledError:
