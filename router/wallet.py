4266f702562e95b30748d937ad061ea35a8c8b1b
Shroominic <db2002dominic@gmail.com>
2025-08-01 18:46:21 -0300

    fix balance

--- router/wallet.py
+++ router/wallet.py
@@ -1,6 +1,7 @@
 import os
 from typing import Literal
 
+from cashu.core.base import Token
 from cashu.wallet.helpers import deserialize_token_from_string, receive, send
 from cashu.wallet.wallet import Wallet
 
@@ -23,7 +24,8 @@ logger = get_logger(__name__)
 
 CurrencyUnit = Literal["sat", "msat"]
 
-PRIMARY_MINT_URL = os.environ["CASHU_MINTS"].split(",")[0]
+TRUSTED_MINTS = os.environ["CASHU_MINTS"].split(",")
+PRIMARY_MINT_URL = TRUSTED_MINTS[0]
 
 
 async def get_balance(unit: CurrencyUnit) -> int:
@@ -34,7 +36,7 @@ async def get_balance(unit: CurrencyUnit) -> int:
         load_all_keysets=True,
         unit=unit,
     )
-    wallet.load_proofs()
+    await wallet.load_proofs()
     return wallet.available_balance.amount
 
 
@@ -49,6 +51,10 @@ async def recieve_token(
         load_all_keysets=True,
         unit=token_obj.unit,
     )
+    if token_obj.mint in TRUSTED_MINTS and token_obj.mint != PRIMARY_MINT_URL:
+        return await swap_to_primary_mint(token_obj, wallet)
+    elif token_obj.mint not in TRUSTED_MINTS:
+        raise ValueError("Mint URL is not supported by this proxy")
     await receive(wallet, token_obj)
     return token_obj.amount, token_obj.unit, token_obj.mint
 
@@ -64,6 +70,32 @@ async def send_token(
     )
     balance, token = await send(wallet, amount=amount, lock="", legacy=False)
     return token
+
+
+async def swap_to_primary_mint(
+    token_obj: Token, wallet: Wallet
+) -> tuple[int, CurrencyUnit, str]:
+    print(f"swap_to_primary_mint, token_obj: {token_obj}")
+    if token_obj.unit == "sat":
+        amount_msat = token_obj.amount * 1000
+    elif token_obj.unit == "msat":
+        amount_msat = token_obj.amount
+    else:
+        raise ValueError("Invalid unit")
+    estimated_fee_sat = max(amount_msat // 1000 * 0.01, 2)
+    amount_msat_after_fee = amount_msat - estimated_fee_sat * 1000
+    mint_quote = await wallet.mint_quote(amount_msat_after_fee, "sat")
+    melt_quote = await wallet.melt_quote(mint_quote.request, amount_msat_after_fee)
+    _ = await wallet.melt(
+        proofs=token_obj.proofs,
+        invoice=mint_quote.request,
+        fee_reserve=melt_quote.fee_reserve,
+        quote_id=melt_quote.quote,
+    )
+
+    _ = await wallet.mint(token_obj.amount, mint_quote.quote)
+
+    return token_obj.amount, "sat", PRIMARY_MINT_URL
 
 
 # insert initial token state here to reduce db calls
